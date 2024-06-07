64fa740d3ef660c0c41a7fc3e6ed7ddde2ca4e74
Aman Singh <amansinghmarch16@gmail.com>
2024-06-07 11:12:34 +0530

    updated Ui

--- components/ProfilePicture.js
+++ components/ProfilePicture.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 const ProfilePicture = () => {
     return (
-        <div className='dp sm:mt-12'>
+        <div className='dp sm:mt-6'>
             <Image height={650} width={800} src={"/profile-pic.png"} alt='dp' />
         </div>
     )
