173752338c215806c1442d90512a31c5c669b35c
Aman Singh <amansinghmarch16@gmail.com>
2024-12-27 01:36:19 +0530

    profile picture changed

--- components/ProfilePicture.js
+++ components/ProfilePicture.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Image from 'next/image'
 const ProfilePicture = () => {
     return (
-        <div className='dp sm:mt-6'>
-            <Image height={650} width={800} src={"/profile-pic.png"} alt='dp' />
+        <div className='dp  sm:mt-6'>
+            <Image height={650} width={800} src={"/dp4.jpg"} alt='dp' />
         </div>
     )
 }
